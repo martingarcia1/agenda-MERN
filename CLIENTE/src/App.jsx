43b3262b17bff0ef4c249aba1aba1e1f46c42ba7
martingarcia1 <martigarcia222@gmail.com>
2024-12-15 15:42:44 -0300

    login

--- CLIENTE/src/App.jsx
+++ CLIENTE/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Datos from './Datos'
 import Editar from './Editar'
 import Listar from './Listar'
@@ -6,7 +6,71 @@ import Listar from './Listar'
 function App() {
   const [contacto, setContacto] = useState(null)
   const [contactos, setContactos] = useState([])
+  let [mensaje, setMensaje] = React.useState('')
+  let [user, setUser] = React.useState('')
+  let [password, setPassword] = React.useState('')
+  let [showRegistro, setShowRegistro] = React.useState(true)
+  let [showLogin, setShowLogin] = React.useState(false)
+  let [showInfo, setShowInfo] = React.useState(false)
+  let [loginUser, setLoginUser] = React.useState('')
+  let [loginPassword, setLoginPassword] = React.useState('')
 
+  async function registro(e) {
+    e.preventDefault()
+    let res = await fetch('/registro', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user, password })
+    });
+    let data = await res.text()
+    setMensaje(data)
+    if (res.ok) {
+      setUser('');
+      setPassword('');
+      setShowLogin(true);
+      setShowRegistro(false);
+    }
+  }
+
+  async function login(e) {
+    e.preventDefault();
+    let res = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user: loginUser, password: loginPassword })
+    });
+    let data = await res.text();
+    setMensaje(data.mensaje)
+    if (res.ok) {
+      setShowLogin(false);
+      setShowInfo(true);
+    }
+
+  }
+
+  async function info() {
+    let res = await fetch('/privado', {
+      method: 'GET',
+      credentials: 'include',
+    })
+    let data = await res.text();
+    setMensaje(data)
+  }
+
+  async function logout() {
+    let res = await fetch('/logout', {
+      method: 'PUT',
+      credentials: 'include',
+    })
+    let data = await res.text();
+    setMensaje(data.mensaje);
+    setShowInfo(false);
+    setShowLogin(true);
+  }
   async function cargar() {
     let lista = await Datos.listar()
     setContactos(lista)
@@ -34,14 +98,73 @@ function App() {
   }
 
   return (
-    <>
-      {contacto ?
-        <Editar contacto={contacto} alActualizar={actualizar} /> :
-        <Listar contactos={contactos} alAgregar={agregar} alBorrar={borrar} />
-      }
-      
-    </>
-  )
+    <div>
+      <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#fff', borderBottom: '1px solid #ccc' }}>
+        <h1 style={{ margin: 0 }}>AgendaPro</h1>
+        {!showInfo && (
+          <div>
+            {showLogin || showRegistro ? (
+              <>
+                <button onClick={() => { setShowRegistro(true); setShowLogin(false); }}>Registrar</button>
+                <button onClick={() => { setShowRegistro(false); setShowLogin(true); }}>Ingresar</button>
+              </>
+            ) : null}
+          </div>
+        )}
+        {showInfo && (
+          <button onClick={logout}>Cerrar sesión</button>
+        )}
+      </header>
+
+      <main>
+        {showRegistro && (
+          <form>
+            <h2>Registro</h2>
+            <section>
+              <label>Crear Nombre de Usuario</label>
+              <input type="text" placeholder="Usuario" value={user} onChange={(e) => setUser(e.target.value)} />
+              <label>Contraseña</label>
+              <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <span>
+                <button type="button" onClick={registro}>Registrarse</button>
+                <button type="button" onClick={() => { setShowRegistro(false); setShowLogin(true); }}>Ya tengo cuenta</button>
+              </span>
+            </section>
+          </form>
+        )}
+
+        {showLogin && (
+          <form>
+            <h2>Login</h2>
+            <section>
+              <label>Nombre de Usuario</label>
+              <input type="text" placeholder="Usuario" value={loginUser} onChange={(e) => setLoginUser(e.target.value)} />
+              <label>Contraseña</label>
+              <input type="password" placeholder="Contraseña" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
+              <span>
+                <button onClick={login}>Iniciar sesión</button>
+                <button onClick={() => { setShowRegistro(true); setShowLogin(false); }}>Registrarse</button>
+              </span>
+            </section>
+          </form>
+        )}
+
+        {showInfo && (
+          <div>
+            <h2>Información privada</h2>
+            <button onClick={info}>Ver información</button>
+            <button onClick={logout}>Cerrar sesión</button>
+            {contacto ? (
+              <Editar contacto={contacto} alActualizar={actualizar} />
+            ) : (
+              <Listar contactos={contactos} alAgregar={agregar} alBorrar={borrar} />
+            )}
+          </div>
+        )}
+        <pre>{mensaje}</pre>
+      </main>
+    </div>
+  );
 }
 
 export default App
--- SERVIDOR/servidor.js
+++ SERVIDOR/servidor.js
@@ -1,13 +1,80 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 import cors from "cors"
 import Agenda from './routers/agenda.js';
 
 const app = express();
 
+app.use(morgan("dev"))
+app.use(cookieParser())
 app.use(express.json());
+app.use(express.static("public"))
 app.use(cors());
 
 app.use("/contactos", Agenda)
+
+let usuarios = []
+
+function tokens() {
+  return Math.random().toString().substring(2);
+}
+
+function validacion(req, res, next) {
+  let token = req.cookies.token
+  let usuario = usuarios.find(usuario => usuario.token === token)
+  if (usuario) {
+    req.usuario = usuario
+    next()
+  } else {
+    res.status(401).send("No estas autorizado")
+  }
+}
+
+//RUTAS
+
+app.post('/registro', (req, res) => {
+  let { user, password } = req.body
+  if (!user || !password) {
+    return res.status(400).json({ error: 'Faltan datos' })
+  }
+
+  let existe = usuarios.find(usuario => usuario.user === user)
+  if (existe) {
+    res.status(402).json({ error: 'El usuario ya existe' })
+  } else {
+    usuarios.push({ user, password })
+    res.send('Usuario registrado correctamente')
+  }
+})
+
+app.post('/login', (req, res) => {
+  let { user, password } = req.body
+  if (!user || !password) {
+    return res.status(400).json({ error: 'Usuario o Contraseña invalidos' })
+  }
+
+  let usuario = usuarios.find(usuario => usuario.user === user && usuario.password === password)
+  if (usuario) {
+    let token = tokens()
+    usuario.token = token
+    res.cookie("token", token)
+    return res.json("Logueado correctamente")
+  } else {
+    return res.status(401).json({ error: 'Usuario o Contraseña invalidos' })
+  }
+})
+
+app.put('/logout', validacion, (req, res) => {
+  let usuario = req.usuario
+  delete usuario.token
+  res.clearCookie('token')
+  res.json('Sesion Cerrada')
+}) 
+
+app.get('/privado', validacion, (req, res) => {
+  res.json('Esta es una ruta privada, solo para usuarios logueados')
+})
 
 app.listen(3000, () => {
   console.log("Servidor iniciado en http://localhost:3000")
